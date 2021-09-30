146784a24a2a6862376b27390e8330076defcf98
Intan9 <intanroktaviani@gmail.com>
2021-09-30 12:20:29 +0700

    Latihan 2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Halo, Perkenalkan</h1>";
+        echo "Nama saya Intan Restu Oktaviani, saya tinggal di daerah Ciawi. Olahraga yang saya sukai yaitu bermain badminton,";
+        
+    }
+}
